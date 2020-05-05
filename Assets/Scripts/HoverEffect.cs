4d29cb23224fdd329d61aa1d02dbfcf1c314978b
Hélain Schoonjans <helain90@hotmail.com>
2020-05-05 08:51:59 +0200

    Added an hover effect on game items.

---
+++ Assets/Scripts/HoverEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverEffect : MonoBehaviour
+{
+    public float hoverAmount;
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("Hover effect triggered");
+        transform.localScale += Vector3.one * hoverAmount;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale -= Vector3.one * hoverAmount;
+    }
+
+}
