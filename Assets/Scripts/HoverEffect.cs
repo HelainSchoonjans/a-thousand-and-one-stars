f2f1a95e0e9f2d427fab339a1c2a750c3b897f9f
Hélain Schoonjans <helain90@hotmail.com>
2020-05-11 08:33:24 +0200

    Blocks hover and click on elements when the dialog box is open

--- Assets/Scripts/HoverEffect.cs
+++ Assets/Scripts/HoverEffect.cs
@@ -6,13 +6,26 @@ public class HoverEffect : MonoBehaviour
 {
     public float hoverAmount;
 
+    private bool IsPointingOverUI()
+    {
+        return UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
+    }
+
     private void OnMouseEnter()
     {
+        if(IsPointingOverUI())
+        {
+            return;
+        }
         transform.localScale += Vector3.one * hoverAmount;
     }
 
     private void OnMouseExit()
     {
+        if (IsPointingOverUI())
+        {
+            return;
+        }
         transform.localScale -= Vector3.one * hoverAmount;
     }
 
