b350e4cecfe8c68fed14e5bb3cee9e77579df658
Hélain Schoonjans <helain90@hotmail.com>
2020-05-23 17:28:46 +0200

    Linked the scores with the selected player's scores

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,13 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class GameMaster : MonoBehaviour
 {
+    public TextMeshProUGUI reputationScoreText;
+    public TextMeshProUGUI experienceScoreText;
+    public TextMeshProUGUI wealthScoreText;
+    public Player currentPlayer;
+
+    public void UpdatePlayerScores()
+    {
+        if(currentPlayer)
+        {
+            reputationScoreText.text = currentPlayer.reputation.ToString();
+            experienceScoreText.text = currentPlayer.experience.ToString();
+            wealthScoreText.text = currentPlayer.wealth.ToString();
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdatePlayerScores();
     }
 
     // Update is called once per frame
